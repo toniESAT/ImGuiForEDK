0204ab1ca5d56b1951837f44b18037de895acd9d
Toni M. Brotons <martinezbro@esat-alumni.com>
2025-04-09 11:40:51 +0200

    Remove GLFW window as a parameter

--- edk_imgui.h
+++ edk_imgui.h
@@ -10,9 +10,10 @@
 
 using namespace EDK3ImGui;
 
-inline void ImGuiInit(GLFWwindow* window, float window_width, float window_height) {
+inline void ImGuiInit(float window_width, float window_height) {
   EDK3ImGui::ImGuiContext* ctx = ImGui::CreateContext();
   ImGui::SetCurrentContext(ctx);
+  GLFWwindow* window = glfwGetCurrentContext();
   ImGuiIO& io = ImGui::GetIO();
   io.DisplaySize = ImVec2(window_width, window_height);
   ImGui_ImplGlfw_InitForOpenGL(window, true);
@@ -29,14 +30,16 @@ inline void ImGuiDraw() {
   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
-inline void WindowMakeResizable(GLFWwindow* window) {
+inline void WindowMakeResizable() {
+  GLFWwindow* window = glfwGetCurrentContext();
   HWND hwnd = glfwGetWin32Window(window);
   LONG_PTR style = GetWindowLongPtr(hwnd, GWL_STYLE);
   style |= WS_SIZEBOX | WS_MAXIMIZEBOX | WS_THICKFRAME;
   SetWindowLongPtr(hwnd, GWL_STYLE, style);
 }
 
-inline void WindowMakeBorderless(GLFWwindow* window) {
+inline void WindowMakeBorderless() {
+  GLFWwindow* window = glfwGetCurrentContext();
   HWND hwnd = glfwGetWin32Window(window);
   LONG_PTR style = GetWindowLongPtr(hwnd, GWL_STYLE);
   style &= ~(WS_CAPTION | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX |
